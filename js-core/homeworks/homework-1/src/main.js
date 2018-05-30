c5359e568aa01e7dc63a80e8edb75345c4ff0315
ilya <ilya.gil@vectaury.io>
2018-05-30 15:56:51 +0300

    # таск компания: комментарий

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,19 @@
+
+var user = {
+    firstName: 'olga',
+    secondName: 'kuzmina',
+    phoneNumber: '0664133296',
+    email: null,
+    address: {
+        city: 'kharkov',
+        street: 'Ahsarova',
+        homeNumber: '13a',
+        apartNumber: 12
+    },
+    linkedAccounts: [user, user, user],
+    defaultRingtone: true
+};
+
+
+
+console.log(user);
