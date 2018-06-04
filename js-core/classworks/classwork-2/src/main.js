f87c94bd3b535daae491f4a7177ecf1cbec26f8b
ilya <ilya.gil@vectaury.io>
2018-06-04 10:23:34 +0300

    changed homework

---
+++ js-core/classworks/classwork-2/src/main.js
@@ -0,0 +1,10 @@
+const myName = `olga kuzmina`;
+const myNameUppercase = myName.toUpperCase();
+
+const student = {
+    skills: ['js', 'php'],
+    salary: '2000$'
+};
+student.salary = '3000$';
+
+console.log(student);
--- js-core/homeworks/homework-1/src/main.js
+++ js-core/homeworks/homework-1/src/main.js
@@ -1,17 +1,21 @@
+var outCall;
 
 var user = {
     firstName: 'olga',
     secondName: 'kuzmina',
     phoneNumber: '0664133296',
     email: null,
+    index: NaN,
     address: {
         city: 'kharkov',
         street: 'Ahsarova',
         homeNumber: '13a',
         apartNumber: 12
     },
-    linkedAccounts: [user, user, user],
-    defaultRingtone: true
+    linkedAccounts: ['max', 'father', 'mother'],
+    defaultRingtone: true,
+    outCall,
+
 };
 
 
