6cd1528941e565981b4642ddfbcf8b039ce92fd5
olgakuzminagithub <olga.kuzmina1989@gmail.com>
2018-07-26 16:56:31 +0300

    changes on remarks

--- phoneApp/js/main.js
+++ phoneApp/js/main.js
@@ -1,3 +1,69 @@
+class ContactPage {
+    constructor (colums, users) {
+        this.colums = colums;
+        this.users = users;
+    }
+    render() {
+        const mountNode = document.createElement('div');
+        mountNode.setAttribute('id', 'mountNode');
+        mountNode.innerHTML += this.renderHeader() +this.renderMain() + this.renderFooter();
+        document.body.appendChild(mountNode);
+    }
+    renderHeader () {
+        return `<header class="header">
+            <div class="container top-radius">
+                <h2>Contacts</h2>
+        </div>
+        </header>`
+    }
+    renderMain () {
+        return `<main>
+        <div class="container">
+         <form class="form-inline search-form">
+            <div class="form-group">
+                <label class="sr-only" for="search">Search</label>
+                <input type="text" class="form-control" id= "search" placeholder="Search">
+            </div>
+         </form>
+         <table class="table table-hover contacts">
+         <thead>
+          ${this.colums.map(colum => {return`<th>${colum}</th>`}).join('')}
+         </thead>
+         <tbody>
+         ${this.users.map(user => {return`
+             <tr>
+             <td>${user.name}</td>
+             <td>${user.lastName}</td>
+             <td>${user.email}</td>
+             </tr>`
+        }).join('')}</tbody>
+         </table>
+         </div>
+         </main>`
+    }
+    renderFooter () {
+        return `<footer class="footer">
+        <div class="container bottom-radius">
+        <nav class="main-nav">
+            ${this.renderLink({ url: 'contacts.html', content:'Contacts', className:'active', icon:'search'})}
+            ${this.renderLink({ url: 'keypad.html', content:'Keypad',  icon:'th'})}
+            ${this.renderLink({ url: 'edit-contact.html', content:'Edit contact',  icon:'pencil'})}
+            ${this.renderLink({ url: 'user.htm', content:'User',  icon:'user'})}
+            ${this.renderLink({ url: 'add-user.html', content:'Add user',  icon:'plus'})}
+         </nav>
+         </div>
+         </footer>`
+
+    }
+    renderLink (linkProperties) {
+        return `<a href="${linkProperties.url}.html" class="tab ${linkProperties.className}">
+                <span class="glyphicon glyphicon-${linkProperties.icon}" aria-hidden="true"></span>
+                <span class = "tab-text">${linkProperties.content}</span>
+         </a>`
+
+    }
+}
+
 const users = [
     {
         name: 'Иван',
@@ -39,81 +105,24 @@ const users = [
         lastName: 'Винник',
         email: 'smirnov@gmail.com'
     },
+    {
+        name: 'Паша',
+        lastName: 'Новиков',
+        email: 'pasha@gmail.com'
+    },
+    {
+        name: 'Даша',
+        lastName: 'Стрижак',
+        email: 'dasha@gmail.com'
+    },
+    {
+        name: 'Елена',
+        lastName: 'Лещенко',
+        email: '@gmail.com'
+    },
 ];
 const colums = ['Name', 'LastName', 'Email'];
-const app  = {
-    users,
-    colums,
-    render() {
-        document.body.innerHTML += this.renderHeader() +this.renderMain() + this.renderFooter();
-    },
-    renderHeader () {
-        return `
-        <header class="header">
-            <div class="container top-radius">
-                <h2>Contacts</h2>
-        </div>
-        </header>
-        `
-    },
-    renderMain () {
-        return `
-        <main>
-        <div class="container">
-         <form class="form-inline search-form">
-            <div class="form-group">
-                <label class="sr-only" for="search">Search</label>
-                <input type="text" class="form-control" id= "search" placeholder="Search">
-            </div>
-         </form>
-         <table class="table table-hover contacts">
-         <thead>
-          ${this.colums.map(colum => {return`<th>${colum}</th>`}).join('')}
-         </thead>
-         <tbody>
-         ${this.users.map(user => {return`
-             <tr>
-             <td>${user.name}</td>
-             <td>${user.lastName}</td>
-             <td>${user.email}</td>
-             </tr>`
-         }).join('')}</tbody>
-         </table>
-         </div>
-         </main>`
-    },
-    renderFooter () {
-        return `
-        <footer class="footer">
-        <div class="container bottom-radius">
-        <nav class="main-nav">
-            <a href="index.html" class="tab active">
-                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                <span class = "tab-text">Contacts</span>
-            </a>
-            <a href="keypad.html" class="tab">
-                <span class="glyphicon glyphicon-th" aria-hidden="true"></span>
-                <span class = "tab-text">Keypad</span>
-            </a>
-            <a href="edit-contact.html" class="tab">
-                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                <span class = "tab-text">Edit contact</span>
-            </a>
-            <a href="user.html" class="tab">
-                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                <span class = "tab-text">User</span>
-            </a>
-            <a href="add-user.html" class="tab">
-                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                <span class = "tab-text">Add user</span>
-            </a>
-         </nav>
-         </div>
-         </footer>`
 
-    }
-
-};
-
-app.render();
+const contactPage = new ContactPage(colums, users);
+contactPage.render();
 
