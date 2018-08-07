f286a7985f7d333c409d0b939ce863f933a69d83
olgakuzminagithub <olga.kuzmina1989@gmail.com>
2018-08-07 12:33:07 +0300

    TASK 2 for phoneApp

--- phoneApp/js/keypad.js
+++ phoneApp/js/keypad.js
@@ -47,14 +47,34 @@ class KeypadPage {
         return `<footer class="footer">
         <div class="container bottom-radius">
         <nav class="main-nav">
-            ${this.renderLink({ url: 'contacts.html', content:'Contacts', className:'active', icon:'search'})}
-            ${this.renderLink({ url: 'keypad.html', content:'Keypad',  icon:'th'})}
+            ${this.renderLink({ url: 'contacts.html', content:'Contacts', icon:'search'})}
+            ${this.renderLink({ url: 'keypad.html', content:'Keypad', className:'active',  icon:'th'})}
             ${this.renderLink({ url: 'edit-contact.html', content:'Edit contact',  icon:'pencil'})}
             ${this.renderLink({ url: 'user.htm', content:'User',  icon:'user'})}
             ${this.renderLink({ url: 'add-user.html', content:'Add user',  icon:'plus'})}
          </nav>
          </div>
          </footer>`
+
+    }
+    conversionPhoneFormat (phoneNumber) {
+        switch(phoneNumber.length) {
+            case 1: return phoneNumber.replace(/([0-9]{1})/, '($1)');
+            case 2: return phoneNumber.replace(/([0-9]{2})/, '($1)');
+            case 3: return phoneNumber.replace(/([0-9]{3})/, '($1)');
+            case 4: return phoneNumber.replace(/([0-9]{3})([0-9]{1})/, '($1) $2'); break;
+            case 5: return phoneNumber.replace(/([0-9]{3})([0-9]{2})/, '($1) $2'); break;
+            case 6: return phoneNumber.replace(/([0-9]{3})([0-9]{2})([0-9]{1})/, '($1) $2-$3'); break;
+            case 7: return phoneNumber.replace(/([0-9]{3})([0-9]{2})([0-9]{2})/, '($1) $2-$3'); break;
+            case 8: return phoneNumber.replace(/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{1})/, '($1) $2-$3-$4'); break;
+            case 9: return phoneNumber.replace(/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{2})/, '($1) $2-$3-$4'); break;
+            case 10: return phoneNumber.replace(/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{3})/, '($1) $2-$3-$4'); break;
+            default: break;
+        }
+    }
+    deleteSimbol (phoneNumber) {
+        return phoneNumber.replace(/\D/g, "");
+
 
     }
 }
@@ -69,8 +89,9 @@ const entryField = document.querySelector('.numbers');
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
-        if (entryField.textContent.length < 10) {
-            entryField.innerHTML += `${buttons[i].textContent}`;
+        if ( keypadPage.deleteSimbol(entryField.textContent).length < 10) {
+            entryField.textContent = keypadPage.deleteSimbol(entryField.textContent) +`${buttons[i].textContent}`;
+            entryField.innerHTML = keypadPage.conversionPhoneFormat(entryField.textContent);
         }
     }
 }
@@ -81,4 +102,30 @@ const buttonDelete = document.querySelector('.glyphicon-circle-arrow-left');
 
 buttonDelete.onclick = function () {
     entryField.innerHTML = `${entryField.textContent.slice(0, -1)}`;
+};
+
+
+/*Набор с клавиатуры */
+
+function getChar(event) {
+    console.log(event.which);
+    if (event.which == null) { // IE
+        if (event.keyCode < 32) return null; // спец. символ
+        return String.fromCharCode(event.keyCode)
+    }
+
+    if (event.which != 0 && event.charCode != 0) { // все кроме IE
+        if (event.which < 32) return null; // спец. символ
+        return String.fromCharCode(event.which); // остальные
+    }
+
+    return null; // спец. символ
+}
+
+
+document.onkeypress = function (event) {
+    if ( keypadPage.deleteSimbol(entryField.textContent).length < 10) {
+        entryField.textContent = keypadPage.deleteSimbol(entryField.textContent) + getChar(event);
+        entryField.innerHTML = keypadPage.conversionPhoneFormat(entryField.textContent);
+    }
 };
